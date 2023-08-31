43a3b83e4a95bc49a0bcb9f4bf1e72573de89b5e
Zhutianyi7230 <1589841300@qq.com>
2023-08-31 14:52:37 +0800

    2023-8-31

--- tools/demo_quant.py
+++ tools/demo_quant.py
@@ -270,7 +270,7 @@ def detect(cfg,opt):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='./model_int8_quant.pth', help='model.pth path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='./weights/model_int8_quant.pth', help='model.pth path(s)')
     parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder   ex:inference/images
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
